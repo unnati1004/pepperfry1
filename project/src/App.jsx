d0baf5e61544359c738be60aa85918a60037605c
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-04-03 21:36:58 +0530

    project

---
+++ project/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+import React from 'react'
+//import Footer from './components/footer/Footer.jsx'
+import { AllRoutes } from './routers/Routers.jsx'
+
+export default function App() {
+  return (
+    <div>
+    <AllRoutes />
+    </div>
+  )
+}
+
+
